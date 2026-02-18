6ac66ed9e7c2d8976f67f1242d43179965881713
msn430 <30yan.ka0430@gmail.com>
2026-02-18 14:29:44 +0330

    implement view layout

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -71,49 +71,9 @@
                 </a>
             </div>
         </div>
-        <!-- NAVBAR -->
-        <div class="relative flex-between h-16 bg-gray-900 dark:bg-gray-800 rounded-full text-gray-200 px-10">
-            <!-- MENU -->
-            <ul class="flex items-center gap-x-8">
-                <li class="menu-item">
-                    <a href="http://127.0.0.1:8000" class="menu-item_link">
-                        صفحه اصلی
-                    </a>
-                </li>
 
-                <li class="menu-item">
-                    <a href="http://127.0.0.1:8000/products" class="menu-item_link">
-                        فروشگاه
-                    </a>
-                </li>
+        @include('layout.navbar')
 
-                <li class="menu-item">
-                    <a href="/" class="menu-item_link">
-                        دسته بندی ها
-                    </a>
-                </li>
-
-                <li class="menu-item">
-                    <a href="/" class="menu-item_link">
-                        تماس با ما
-                    </a>
-                </li>
-
-                <li class="menu-item">
-                    <a href="/" class="menu-item_link">
-                        سوالات متداول
-                    </a>
-                </li>
-
-                <li class="menu-item">
-                    <a href="/" class="menu-item_link">
-                        درباره ما
-                    </a>
-                </li>
-
-            </ul>
-
-        </div>
     </div>
     <!-- Mobile -->
     <div class="flex justify-center lg:hidden">
